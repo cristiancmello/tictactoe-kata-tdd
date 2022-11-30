957e6c0e22f6e56c9f438b000939dc287d30833a
Cristian Mello <cristianc.mello@gmail.com>
2022-11-30 00:00:39 -0300

    Adicionando quandoPecaColocadaLugarForaEixoX_entaoPosicaoInvalida

--- src/test/java/org/tictactoe/TicTacToeTests.java
+++ src/test/java/org/tictactoe/TicTacToeTests.java
@@ -3,10 +3,15 @@ package org.tictactoe;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TicTacToeTests {
     @Test
-    void testing() {
-        assertThat(true).isTrue();
+    void quandoPecaColocadaLugarForaEixoX_entaoPosicaoInvalida() {
+        // Primeiro passo: criar um objeto? Não, criar um teste, uma asserção. Recomendado por Kent Beck.
+        // Kent Beck criou o JUnit...
+        // STEP RED: teste precisa falhar obrigatoriamente. Teste criado já passando pode estar indicando falso positivo
+        // ou confirmando vieses de implementação
+        assertThrows(RuntimeException.class, () -> {}, "Posicao Invalida");
     }
 }
