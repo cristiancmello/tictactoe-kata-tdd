32d80ec4ee7729d69f7ac397bd71157b413a6cbc
Cristian Mello <cristianc.mello@gmail.com>
2022-11-30 16:46:26 -0300

    adicionando teste

--- src/test/java/org/tictactoe/TicTacToeTests.java
+++ src/test/java/org/tictactoe/TicTacToeTests.java
@@ -129,4 +129,15 @@ public class TicTacToeTests {
         String atual = tictactoe.play(3, 3);
         assertThat(atual).isEqualTo("X é o vencedor");
     }
+
+    @Test
+    void quandoJogarEPreencherTodaDiagonalBaixoCima_entaoVence() {
+        tictactoe.play(1, 3);
+        tictactoe.play(1, 1);
+        tictactoe.play(2, 2);
+        tictactoe.play(1, 2);
+
+        String atual = tictactoe.play(3, 1);
+        assertThat(atual).isEqualTo("X é o vencedor");
+    }
 }
