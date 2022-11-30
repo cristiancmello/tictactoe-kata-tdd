b996b53f12f5dca9bb9412ea1b30fc442484cde8
Cristian Mello <cristianc.mello@gmail.com>
2022-11-29 23:51:14 -0300

    Criando projeto com suporte a jacoco e junit5

---
+++ src/test/java/org/tictactoe/TicTacToeTests.java
@@ -0,0 +1,12 @@
+package org.tictactoe;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class TicTacToeTests {
+    @Test
+    void testing() {
+        assertThat(true).isTrue();
+    }
+}
